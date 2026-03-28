b1c58eea50b8aae5d0fc132f1babfb96f77de913
Raghav Baghla <raghavbaghla.cse@gmail.com>
2026-03-28 14:31:48 +0530

    initialized frontend

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", function (socket) {
+    socket.on("message", (e) => {
+        if (e.toString() === "ping")
+            socket.send("pong");
+    });
+});
+//# sourceMappingURL=index.js.map
